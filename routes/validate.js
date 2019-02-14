0422813ef86f1982ae293fb244e9dc28a88583b4
Jayden <676081195@qq.com>
2019-02-14 18:21:44 +0800

    Fix listen ip to 0.0.0.0

--- routes/validate.js
+++ routes/validate.js
@@ -33,7 +33,7 @@ router.route('/login')
         res.status(200)
           .cookie('token', 'tokenID', {
             expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-          })
+           })
           .send({
             msg: 'Login succeed'
           })
