911065ca121ecb93e2b9bfdf2aa47d4ff227492e
Jayden <dqy676081195@gmail.com>
2019-03-18 00:17:36 +0800

    ADD:index page template

--- app.js
+++ app.js
@@ -2,41 +2,51 @@
 require('./models/mongoose')
 
 const express = require('express')
+const nunjucks = require('nunjucks')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const lessMiddleware = require('less-middleware')
 const jwt = require('jsonwebtoken')
 
 // Custom controllers
+const index = require('./routes/index')
 const validateRouter = require('./routes/validate')
 
 const app = express()
-
-// JWT authentication
-app.use(function (req, res, next) {
-  const unless = ['/register', '/login']
-  let isNeedVerify = true
-  unless.forEach(path => {
-    if (req.originalUrl.indexOf(path) > -1) {
-      isNeedVerify = false
-    }
-  })
-
-  if (isNeedVerify) {
-    jwt.verify(req.headers['authorization'], 'secret', function (err, decode) {
-      if (err) {
-        res.status(401).send({
-          msg: 'Invalid token...'
-        })
-      } else {
-        res.locals.username = decode.username
-        return next()
-      }
-    })
-  } else {
-    next()
-  }
+app.use(lessMiddleware(__dirname + '/public', { force: true }))
+// Template engine
+nunjucks.configure('views', {
+  noCache: true,
+  express: app
 })
+app.set('view engine', 'njk');
+app.set('views', path.resolve(__dirname, './views'));
+// JWT authentication
+// app.use(function (req, res, next) {
+//   const need = ['/write']
+//   let isNeedVerify = false
+//   need.forEach(path => {
+//     if (req.originalUrl.indexOf(path) > -1) {
+//       isNeedVerify = false
+//     }
+//   })
+//
+//   if (isNeedVerify) {
+//     jwt.verify(req.headers['authorization'], 'secret', function (err, decode) {
+//       if (err) {
+//         res.status(401).send({
+//           msg: 'Invalid token...'
+//         })
+//       } else {
+//         res.locals.username = decode.username
+//         return next()
+//       }
+//     })
+//   } else {
+//     next()
+//   }
+// })
 
 // Apply middleware
 app.use(logger('dev'))
@@ -46,6 +56,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Apply custom controllers
+app.use(index)
 app.use(validateRouter)
 
 // catch 404 and forward to error handler
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/')
+  .get((req, res) => {
+    res.render('index',{
+      name:'Jayden'
+    })
+  })
+
+module.exports = router
